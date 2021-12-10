c8bc4161cf3f83ad8dec9f31ebb113097e18bcb5
coblanplus <coblanplus@gmail.com>
2021-12-10 10:16:02 +0800

    dd

--- ex/validator.js
+++ ex/validator.js
@@ -75,6 +75,16 @@ export function get_validator(){
                     return msg
                 }
             }
+        },
+        isChinesePhone(msg){
+
+            return function (value){
+                var valid_value = value.replace('+86-','')
+                if(! /^1(3\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\d|9[0-35-9])\d{8}$/.test(valid_value)){
+                    return msg
+                }
+            }
+
         }
     }
 
