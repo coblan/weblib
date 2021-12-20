6953bf0bb5cf6d8209b23a9720ec3a02a47e9b74
coblan <coblan@163.com>
2021-12-21 01:43:01 +0800

    auto commit

---
+++ pc/messageInfo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div v-for='(row,index) in messages' :key="index">{{row}}</div>
+    </div>
+</template>
+<script>
+
+export default {
+    props:{
+        messages:{}
+    }
+}
+</script>
