75cabd079871b66a0b9b24f297de8866516cb43c
coblan <coblan@163.com>
2021-12-27 02:55:34 +0800

    auto commit

---
+++ uis/echartDiv.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="echarts-div">
+        <div  class="mychart" ></div>
+    </div>
+
+</template>
+<script>
+
+ export default {
+     props:{
+        option:{}
+     },
+     setup(props){
+     },
+     mounted(){
+         this.draw()
+     },
+     methods:{
+         async draw(){
+             var dom = this.$el.querySelector('.mychart')
+             var myChart = echarts.init(dom);
+        
+             // 使用刚指定的配置项和数据显示图表。
+             myChart.setOption(this.option);
+         }
+     }
+ }
+</script>
+
+<style scoped lang="scss">
+    .echart-base{
+        display: inline-block;
+        position: relative;
+    }
+    .mychart{
+        width: 100%;
+        height: 100%;
+    }
+</style>
---
+++ uis/echartsDiv.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="echarts-div">
+        <div  class="mychart"></div>
+    </div>
+
+</template>
+<script>
+
+ export default {
+     props:{
+        option:{}
+     },
+     mounted(){
+
+     },
+     methods:{
+         async draw(option){
+             var dom = this.$el.querySelector('.mychart')
+            // var dom = document.getElementById('mychart')
+             var myChart = echarts.init(dom);
+        
+             // 使用刚指定的配置项和数据显示图表。
+           if(option){
+             myChart.setOption(option);
+           }else{
+             myChart.setOption(this.option);
+           }
+
+         }
+     }
+ }
+</script>
+
+<style scoped lang="scss">
+.echarts-div{
+    display: inline-block;
+    position: relative;
+}
+.mychart{
+    width: 100%;
+    height: 100%;
+}
+</style>
