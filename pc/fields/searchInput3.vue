cff17827b243c0cae5e48c96e87dc84c5ea90413
coblanplus <coblanplus@gmail.com>
2022-01-19 12:57:38 +0800

    dd

--- pc/fields/searchInput3.vue
+++ pc/fields/searchInput3.vue
@@ -1,12 +1,6 @@
 <template>
-  <div class="search-input"  @click.stop>
-
-    <div>
-      <input id="myinput"  type="text" v-model="word" @keyup.enter="onSubmit">
-    </div>
-    <div v-if="!stretched" @click="onStrech" class="search-placeholder">
-      <span style="white-space: nowrap;font-size: 90%">{{placeholder}}</span>
-    </div>
+  <div class="search-input"  >
+    <input id="myinput"  type="text" v-model="word" @keyup.enter="onSubmit" :placeholder="placeholder">
      <img style="position: relative;top:1px;display: inline-block;margin-left: 6px;" @click="onSubmit" class="clickable" :src="require('weblib/assets/search.png')" alt="">
   </div>
 </template>
@@ -15,60 +9,43 @@ export  default  {
   props:{
     placeholder:{
       default:'搜索'
+    },
+    value:{
+
     }
   },
   data(){
     return {
       stretched:false,
-      word:""
+      word:this.value
+    }
+  },
+  watch:{
+    value(nv){
+      this.word = nv
     }
   },
   methods:{
     onSubmit(){
-      if(this.word){
-        this.$emit('search',this.word)
-      }
+      this.$emit('input',this.word)
     },
-    onStrech(){
-      this.stretched = true
-      document.body.addEventListener("click", ()=> {
-        this.stretched = false
-      }, {once : true});
-      // this.$refs.myinput.
-      this.$nextTick(()=>{
-        this.$el.querySelector('#myinput').focus()
-      })
-
-    },
-    onBlur(){
-      this.stretched = false
-    }
   }
 }
 </script>
 <style scoped lang="scss">
 .search-input{
-
+  display:  inline-block;
   display: flex;
-  border-radius: 14px;
+
   background-color: #F6F6F6;
   padding: 5px 8px;
   align-items: center;
 
-  .search-placeholder{
-    color: #8B8B8B;
-    width: auto;
-    opacity: .7;
-  }
   input{
     //transition: all linear .3s;
     border: none;
     outline: none;
     background-color: transparent;
-    width: 0px;
-    &.stretched{
-      width: 200px;
-    }
   }
 }
 //.other-place{
