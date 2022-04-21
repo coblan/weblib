55cee177e9661bcc30cbe78039d2fc7a62ebe3d5
coblanplus <coblanplus@gmail.com>
2022-04-21 18:22:28 +0800

    auto commit

--- mb/phoneWin.vue
+++ mb/phoneWin.vue
@@ -1,36 +1,52 @@
 <template>
-  <div class="my-wrap">
-    <slot></slot>
+  <div class="out-wrap">
+    <div class="inn-wrap">
+      <slot></slot>
+    </div>
   </div>
+
 </template>
 
 <script>
 import ex from 'weblib/ex'
 export default {
     mounted(){
-       
+       this.updateSize()
     },
+  created() {
+    window.addEventListener("resize", this.myEventHandler);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.myEventHandler);
+  },
     methods:{
+      myEventHandler(){
+        this.updateSize()
+      },
         updateSize(){
+          var dom = this.$el.querySelector('.inn-wrap')
                 function fullsize(){
-                    $('#main-panel').width( '100vw')
-                    $('#main-panel').height( '100vh')
-                    cfg.updateSizeConfig()
+                  dom.style.width = '100vw'
+                  dom.style.height = '100vh'
+                  document.documentElement.style.fontSize ='13.33333vw'
+                    // cfg.updateSizeConfig()
                 }
                 function fullheight(){
-                    var hh = window.innerHeight*0.563
-                    $('#main-panel').width( hh +'px')
-                    $('#main-panel').height( window.innerHeight +'px')
-                    $('html').css('font-size',hh * 0.1333)
-            //        $(window).trigger('resize')
-                    cfg.updateSizeConfig()
+                  var hh = window.innerHeight*0.563
+                  dom.style.width = hh +'px'
+                  dom.style.height = window.innerHeight +'px'
+                  // html.style.fontSize= (hh/750)+'px'
+                  document.documentElement.style.fontSize = hh/7.5 +'px'
+                  // $('html').css('font-size',hh * 0.1333)
+
+                    // cfg.updateSizeConfig()
                 }
                 function fullwidth(){
                     var hh = window.innerWidth / 0.563
-                    $('#main-panel').height( hh +'px')
-                    $('#main-panel').width( window.innerWidth +'px')
-            //        $(window).trigger('resize')
-                    cfg.updateSizeConfig()
+                    dom.style.height=  hh +'px'
+                    dom.style.width = window.innerWidth +'px'
+
+                    // cfg.updateSizeConfig()
                 }
                 if(ex.os.isPc && !ex.os.isTablet){
                     fullheight()
@@ -45,4 +61,15 @@ export default {
 }
 
 </script>
+<style>
+.out-wrap{
+  background-color: grey;
+  width: 100vw;
+  height: 100vh;
+}
+.inn-wrap{
+  background-color: white;
+  margin: auto;
+}
+</style>
 
