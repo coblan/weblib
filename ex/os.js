3fa9ba66d26cd93f1336a6e996afed93be331e53
coblanplus <coblanplus@gmail.com>
2022-05-17 16:06:24 +0800

    增加ios判断

--- ex/os.js
+++ ex/os.js
@@ -7,13 +7,15 @@ export  var os = function() {
         isFireFox = /(?:Firefox)/.test(ua),
         isChrome = /(?:Chrome|CriOS)/.test(ua),
         isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
-        isPhone = /(?:iPhone)/.test(ua) && !isTablet,
-        isPc = !isPhone && !isAndroid && !isSymbian;
+        isiPhone = /(?:iPhone)/.test(ua) && !isTablet,
+        isPc = !isiPhone && !isAndroid && !isSymbian,
+        isiPad =/(?:iPad|PlayBook)/.test(ua);
         return {
             isTablet: isTablet,
-            isiPhone: isPhone,
+            isiPhone: isiPhone,
+            isiOS: isiPad|| isiPhone,
             isAndroid : isAndroid,
-            isPc : isPc
+            isPc : isPc,
         };
     // }
 
