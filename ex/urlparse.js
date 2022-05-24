9d24502516c8b4054e81f00d2b6802cad334f7f2
coblanplus <coblanplus@gmail.com>
2022-05-24 13:54:09 +0800

    dd

--- ex/urlparse.js
+++ ex/urlparse.js
@@ -85,6 +85,30 @@ export var urlparse={
             segments: a.pathname.replace(/^\//,'').split('/')
         };
     },
+    urlJoin(url, concat){
+        var url1 = url.split('/');
+        var url2 = concat.split('/');
+        var url3 = [ ];
+        for (var i = 0, l = url1.length; i < l; i ++) {
+            if (url1[i] == '..') {
+                url3.pop();
+            } else if (url1[i] == '.') {
+                continue;
+            } else {
+                url3.push(url1[i]);
+            }
+        }
+        for (var i = 0, l = url2.length; i < l; i ++) {
+            if (url2[i] == '..') {
+                url3.pop();
+            } else if (url2[i] == '.') {
+                continue;
+            } else {
+                url3.push(url2[i]);
+            }
+        }
+        return url3.join('/');
+    }
 
 }
 
