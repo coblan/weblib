1e19dcf7815c0e48d6c3219415379e7cfb99862d
coblanplus <coblanplus@gmail.com>
2022-09-02 18:46:00 +0800

    auto commit

--- ex/file.js
+++ ex/file.js
@@ -50,5 +50,68 @@ export default  {
           })
     },
  
+    readLocalFile({accept='',encode='utf-8'}){
+      return new Promise((resolve,reject)=>{
+          ex.__on_filechange=function(event){
+              let files = event.target.files
+              var loadCount = 0 // 已读取文件数量
+              // 读取单个文件
+              var out_text=''
+              var loadAFile = function(file) {
+                  var reader = new FileReader()
+                  reader.onload = function(e) {
+                      var r = this.result
+                      out_text += r
+                      loadCount++
+                      if (loadCount == files.length) {
+                          console.log('读取完毕')
+                          $('#_hide_file_input').val('')
+                          resolve(out_text)
+                      }
+                  }
+                  reader.readAsText(file, encode) // 以文本形式读取
+
+              }
+              for (var i = 0; i < files.length; i++) {
+                  loadAFile(files[i])
+              }
+
+          }
+
+          if(!window._hide_file_input){
+              $('body').append('<input type="file" id="_hide_file_input" style="display: none" >')
+              $('#_hide_file_input').change(function(event){
+                  ex.__on_filechange(event)
+              })
+              window._director_uploadfile_input=true
+              //if(accept){
+                  $('#_hide_file_input').attr('accept',accept)
+              //}
+              $('#_hide_file_input').click()
+          }else{
+              //if(accept){
+                  $('#_hide_file_inputt').attr('accept',accept)
+              //}
+              $('#_hide_file_input').click()
+          }
+      })
+
+  },
+  saveLocalFile  ({content,blob, filename}) {
+      // 创建隐藏的可下载链接
+      var eleLink = document.createElement('a');
+      eleLink.download = filename;
+      eleLink.style.display = 'none';
+      // 字符内容转变成blob地址
+      if(!blob){
+        var blob = new Blob([content]);
+      }
+      eleLink.href = URL.createObjectURL(blob);
+      // 触发点击
+      document.body.appendChild(eleLink);
+      eleLink.click();
+      // 然后移除
+      document.body.removeChild(eleLink);
+  }
  
 }
