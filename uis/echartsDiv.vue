0f5d435163e5b36d9026cb24fb14d0a6f8b5a86e
coblan <coblan@163.com>
2022-10-20 01:36:22 +0800

    auto commit

--- uis/echartsDiv.vue
+++ uis/echartsDiv.vue
@@ -14,15 +14,22 @@ import ex from 'weblib/ex'
 
      },
      methods:{
+       async clear(){
+          this.myChart.dispose()
+          this.myChart = null
+          await this.init()
+       },
+       async init(){
+         await ex.load_js(cfg.js_lib.echarts)
+         console.log(cfg.js_lib.echarts)
+         if(!this.myChart){
+           var dom = this.$el.querySelector('.mychart')
+           // var dom = document.getElementById('mychart')
+           this.myChart = echarts.init(dom);
+         }
+       },
          async draw(option){
-             await ex.load_js(cfg.js_lib.echarts)
-             console.log(cfg.js_lib.echarts)
-
-           if(!this.myChart){
-             var dom = this.$el.querySelector('.mychart')
-             // var dom = document.getElementById('mychart')
-             this. myChart = echarts.init(dom);
-           }
+            await  this.init()
              // 使用刚指定的配置项和数据显示图表。
            if(option){
              this.myChart.setOption(option);
