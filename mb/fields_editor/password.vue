9445043eb457677a3ce6d66c1d67cf4702baa822
coblanplus <coblanplus@gmail.com>
2022-11-07 15:31:21 +0800

    dd

---
+++ mb/fields_editor/password.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <van-field
+        v-model="row[head.name]"
+        :name="head.name"
+        :label="head.label"
+        type="password"
+        :placeholder="head.placeholder || '请输入密码'"
+    />
+  </div>
+
+</template>
+<script>
+export default {
+  props:{
+    head:[],
+    row:[]
+  }
+}
+</script>
