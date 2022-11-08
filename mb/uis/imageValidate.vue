59d46f57c11ce76e4333fba362ad211f58b3a03e
coblanplus <coblanplus@gmail.com>
2022-11-08 10:36:20 +0800

    dd

---
+++ mb/uis/imageValidate.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="out-wrap">
+    <div style="height: .6rem;width: 100%;text-align: center;font-weight: bold">验证码</div>
+    <div class="my-input">
+      <input  style="flex-grow: 10;border: none;" type="text" placeholder="图形验证码" v-model="vcode">
+      <img style="max-width: 2rem" @click="update_code" :src="img_url" alt="">
+    </div>
+    <div style="display: flex;justify-content: center">
+      <mybtn style="width: 4rem;margin: .3rem auto" @click.native="submit">确定</mybtn>
+<!--      <van-button style="width: 4rem;margin: .3rem auto" @click.native="submit" type="primary" size="small">确定</van-button>-->
+    </div>
+  </div>
+</template>
+<script>
+import  ex from 'weblib/ex'
+import mybtn from '@/components/mybtn.vue'
+export default {
+  components:{
+    mybtn,
+  },
+  props:{
+    image_fun:{}
+  },
+  data(){
+    return {
+      img_url:'',
+      vkey:'',
+      vcode:'',
+    }
+  },
+  async mounted() {
+      this.update_code()
+  },
+  methods:{
+    async update_code(){
+      var resp  = await this.image_fun()
+      this.img_url = resp.image_url
+      this.vkey = resp.vkey
+    },
+    async submit(){
+      var dc = {
+        vcode:this.vcode
+      }
+      var error = await ex.vld.validate(dc,{
+        vcode:[ex.vld.notNull('请输入图形验证码')]
+      })
+      if(error){
+        cfg.toast(error)
+        return
+      }
+      this.$emit('finish',{vkey:this.vkey,vcode:this.vcode})
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.my-input{
+  display: flex;
+  border: 1px solid #c2c2c2;
+  padding: .1rem;
+}
+.out-wrap{
+  width: 6rem;
+  height: 2.6rem;
+  background-color: white;
+  padding: .2rem;
+  border-radius: .1rem;
+}
+</style>
