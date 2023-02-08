3d71b35d26fe6431bfc909067082d4994e5cfa91
coblanplus <coblanplus@gmail.com>
2023-02-08 17:59:30 +0800

    ghj

--- mb/fields_editor/linetext.vue
+++ mb/fields_editor/linetext.vue
@@ -2,6 +2,7 @@
     <div>
         <van-field
             v-model="row[head.name]"
+            :readonly="head.readonly"
             :name="head.name"
             :label="head.label"
             :placeholder="head.placeholder || '请输入'"
