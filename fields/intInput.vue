f13485d2d5a4304b51609de2c046069a017447f7
coblanplus <coblanplus@gmail.com>
2023-02-14 10:07:20 +0800

    intInput.vue

---
+++ fields/intInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <input type="number" v-model="inn_value" @keypress="isNumber($event)" :placeholder="placeholder">
+</template>
+<script>
+export default {
+  props:{
+    value:{},
+    placeholder:{},
+  },
+  data(){
+    return {
+      inn_value:this.value
+    }
+  },
+  watch:{
+    value(nv){
+      this.inn_value = nv
+    },
+    inn_value(nv){
+      this.$emit('input',nv)
+    }
+  },
+  methods:{
+    isNumber(evt){
+      evt = (evt) ? evt : window.event;
+      var charCode = (evt.which) ? evt.which : evt.keyCode;
+      if(charCode== 46){
+        return evt.preventDefault();
+      }
+
+      if ((charCode >= 48 && charCode <= 57) || charCode== 46 || charCode== 45) {
+        if(charCode==46 && this.row[this.head.name].indexOf('.')!=-1){
+          return evt.preventDefault();
+        }else{
+          return true
+        }
+      }else{
+        return evt.preventDefault();
+      }
+    }
+  }
+}
+</script>
