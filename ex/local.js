86eb6363955a6de1a0a24143a97b5e471b517a0c
coblanplus <coblanplus@gmail.com>
2024-10-25 18:12:57 +0800

    auto commit

--- ex/local.js
+++ ex/local.js
@@ -17,6 +17,30 @@ export default {
         }else{
             return def
         }
+    },
+    localSetEx(key,value,mini_second){
+        var value_str = JSON.stringify(value)
+        var dc = {
+            mini_second:mini_second,
+            start:Date.now(),
+            data:value_str
+        }
+        localStorage.setItem(key,JSON.stringify(dc))
+    },
+    localGetEx(key,def=null){
+        var resp = localStorage.getItem(key)
+        if(resp==null){
+            return def
+        }else{
+            var dc = JSON.parse(resp)
+            var now = Date.now()
+            if(now - dc.start <dc.mini_second){
+                return JSON.parse(dc.data)
+            }else{
+                localStorage.removeItem(key)
+                return def
+            }
+        }
     }
 
 }
